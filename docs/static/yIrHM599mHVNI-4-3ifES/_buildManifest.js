24d8b226437b0d21a9ab3dd6dc84aad0bbae9ee7
AlexsanderJob <ivs220436@gmail.com>
2022-10-08 14:32:23 +0300

    Test

--- docs/static/yIrHM599mHVNI-4-3ifES/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a59a38e28b175b1b.css","static/chunks/pages/index-903eea3e81d94c1a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: 'docs'
-}
-
-module.exports = nextConfig
